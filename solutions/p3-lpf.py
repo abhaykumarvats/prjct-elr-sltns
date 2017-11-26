459ae61fe707f7a0b69a4db337d91cead5bf1178
abhaykv04 <AbhayKV04@gmail.com>
2017-11-26 18:41:10 +0530

    Add p3-lpf.py

---
+++ solutions/p3-lpf.py
@@ -0,0 +1,40 @@
+#==============================================================================
+# Problem 4: Largest prime factor
+#==============================================================================
+
+def isPrime(number):
+    """
+    number: an integer
+    returns: True if number is prime, False otherwise
+    """
+    
+    # 1 is not prime
+    if number == 1:
+        return False
+    # 2 and 3 are prime numbers
+    elif number == 2 or number == 3:
+        return True
+    else:
+        # check whether number is divisible by any other number in primes list
+        for prime in primes:
+            if number % prime == 0:
+                # return False if it is divisible
+                return False
+        # if the number is prime, append it to primes list and return True
+        primes.append(number)
+        return True
+
+# helper variables and data structures
+primes = [2, 3]
+givenNum = 600851475143
+# square root of given number
+sqrtNum = int(givenNum**0.5)
+
+# for every number till square root of given number
+for number in range(1, sqrtNum + 1):
+    if givenNum % number == 0 and isPrime(number):
+        # if the number is prime
+        largestPrime = number
+
+# print final result
+print("Answer:", largestPrime)
