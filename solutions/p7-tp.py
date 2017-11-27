44d3d3ed25eef9d7bf76bc683b65190d15876c18
abhaykv04 <AbhayKV04@gmail.com>
2017-11-27 18:43:44 +0530

    Refactor code, optimise solution

--- solutions/p7-tp.py
+++ solutions/p7-tp.py
@@ -8,15 +8,19 @@ def is_prime(number):
     returns: True if number is prime, False otherwise
     """
     
+    # square root of number
+    sqrtNumber = int(number**0.5)
+    
     # for each prime in primes
     for prime in primes:
-        # if number is divisible by a prime in primes list,
-        # return False
-        if number % prime == 0:
+        # if current prime is greater than square root of number,
+        # number is prime
+        if prime > sqrtNumber:
+            return True
+        # else if number is divisible by a prime in primes list,
+        # number is not prime
+        elif number % prime == 0:
             return False
-    
-    # if we have reached here, the number is prime, return True
-    return True
 
 
 # we know for sure that 2 and 3 are primes, so we are starting with them
